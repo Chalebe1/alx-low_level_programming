a9b9f1f1bedd20228fc0daf0796ff17edc64d3c7
chalebe1 <angeloebuks@gmail.com>
2022-09-12 16:58:04 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main function
+ *using puts to disply characters
+ *return (0) Always
+ */
+
+int main(void)
+{
+	puts("programming is like a building multilingual puzzle");
+	return(0);
+	
+}	
