8c94aac8c0b6985a5aa56d0ecead8579a4bdc288
chalebe1 <angeloebuks@gmail.com>
2022-10-02 18:47:14 -0700

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,7 +7,7 @@
  * @b: the byte used to replace n bytes of s
  * @n: number of bytes to be repalaced
  *
- * Return: The areaa pointed to by s. 
+ * Return sucess The areaa pointed to by s. 
  */
 
 char *_memset(char *s, char b, unsigned int n)
