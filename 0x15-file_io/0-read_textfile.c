389d333d652347720811b9c56a350019ad18adca
chalebe1 <angeloebuks@gmail.com>
2023-02-13 05:49:38 -0800

    task0 done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,24 +1,21 @@
-Prototype: ssize_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-		char *str;
-			int o = open(filename, O_RDONLY | 0_WRONLY);
+	char *str;
+	int o = open(filename, O_RDONLY | 0_WRONLY);
 				
-				if (fd==-1 || filename==NULL)
-							return (0);
-						
+	if (fd==-1 || filename==NULL)
+		return (0);		
 					
-					ssize_ r = read(o,str,letters);
-						
-						if (n==-1)
-									return (0);
+	ssize_ r = read(o,str,letters);					
+	if (n==-1)
+		return (0);
 
-							ssize_t w = write(STDOUT_FILENO, str, letters);
+	ssize_t w = write(STDOUT_FILENO, str, letters);
 								
-								if  (w == -1 || w != letters)
-											return (0);
-
-									close(o);
-										 if (r == w)
-											 		return (w);
+	if  (w == -1 || w != letters)
+		return (0);
+	close(o);
+	if (r == w)
+		return (w);
 }
 	
