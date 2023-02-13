b786116a4409b1f7b8935091e9d94e897b6a75bc
chalebe1 <angeloebuks@gmail.com>
2023-02-13 07:36:33 -0800

    task0 done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,20 +19,35 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *str;
 	ssize_t o, r, w;
-	str = malloc(sizeof(char) * letters); 
+
+	if (filename == NULL)
+		return (0);
+
+	str = malloc(sizeof(char) * letters);
+       if (str == NULL)
+       		return (0);
+
 	o=open(filename, O_RDONLY);
 				
-	if (o == -1 || filename == NULL)
-		return (0);		
+	if (o == -1)
+	{
+		free(str);
+		return (0);
+	}	
 					
 	r = read(o,str,letters);					
 	if (r == -1)
+	{
+		free (str);
 		return (0);
+	}
 	w = write(STDOUT_FILENO, str, letters);
 								
 	if  (w == -1 || w != r)
+	{
+		free (str);
 		return (0);
+	}
 	close(o);
 	return (w);
 }
-	
