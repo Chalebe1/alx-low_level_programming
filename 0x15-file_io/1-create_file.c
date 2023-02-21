19deb5c8d9d2ab389415282b7d9a5733c9679f1c
chalebe1 <angeloebuks@gmail.com>
2023-02-20 23:13:13 -0800

    task 1 done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  *  * create_file - creates a file
@@ -8,30 +7,32 @@
  *     *
  *      * Return: 1 if it success. -1 if it fails.
  *       */
-
 int create_file(const char *filename, char *text_content)
 {
-	int o;
-	ssize_t w;
-	ssize_t len;
+	int fd;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	if (text_content == NULL)
-		o = open(filename,O_CREAT | O_TRUNC, 0600);
-			if (o == -1)
-				return (-1);
-		return (1);
-	o = open(filename, O_CREAT | O_WRONLY | O_EXCL | O_TRUNC ,0600);
-	if (o == -1)
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
 		return (-1);
-	len = strlen(text_content);
-	w = write(o, text_content, len);
-		if (w != len)
-		{
-			close(o);
-			return (-1);
-		}
-	close(o);
+
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
 	return (1);
-}	
+}
