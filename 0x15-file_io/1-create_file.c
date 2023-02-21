efca3bb4fae06f3dbb2e32233ef49602b40e44b8
chalebe1 <angeloebuks@gmail.com>
2023-02-20 20:43:17 -0800

    task 1 done

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+int create_file(const char *filename, char *text_content)
+{
+	int o,w;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		o = open(filename,O_CREAT | O_TRUNC, 0600);
+			if (o == -1)
+				return (-1);
+		return (1);
+	o = open(filename, O_CREAT | O_WRONLY | O_EXCL | O_TRUNC ,0600);
+	if (o == -1)
+		return (-1);
+	w = write(o,text_content,strlen(text_content));
+		if (w == -1)	
+			return (-1);
+
+	return (1);
+}	
