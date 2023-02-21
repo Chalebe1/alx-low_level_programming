1b2753efec13cab193d81f2a1256c572d59c86af
chalebe1 <angeloebuks@gmail.com>
2023-02-20 23:51:39 -0800

    correct output

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,43 @@
 #include "main.h"
 
 /**
- *  * create_file - creates a file
- *   * @filename: filename.
- *    * @text_content: content writed in the file.
- *     *
- *      * Return: 1 if it success. -1 if it fails.
- *       */
+ *create_file - creates a file
+ *@filename: filename.
+ *@text_content: content writed in the file.
+ *
+ *Return: 1 if it success. -1 if it fails.
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
-
-	if (!filename)
+	int o;
+	ssize_t w;
+	ssize_t len = 0;
+	
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (text_content == NULL)
+	{
+		o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+		if (o == -1)
+			return (-1);
+		return (1);
+	}
+	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fd == -1)
+	if (o == -1)
 		return (-1);
+													        
+	for (len = 0; text_content[len];)
+		len++;
 
-	if (!text_content)
-		text_content = "";
+	w = write(o, text_content, len);
 
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
-
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
+	if (w != len)
+	{
+		close(o);
 		return (-1);
-
-	close(fd);
-
+	}
+	close(o);
 	return (1);
 }
