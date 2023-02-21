dcfae86aefdb499b2668435b6969d9160e0b99cc
chalebe1 <angeloebuks@gmail.com>
2023-02-21 04:52:54 -0800

    task2 done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,14 +7,14 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	o=open(filename, O_EXCL | O_APPEND);
+	o = open(filename, O_EXCL | O_APPEND);
 	if (o == -1)
 		return (-1);
 	if (text_content == NULL)
 		return (-1);
 	for (len = 0; text_content[len];)
 		len++;
-	w=write(o, text_content, len);
+	w = write(o, text_content, len);
 	if (w != len)
 		return (-1);
 	return (1);
