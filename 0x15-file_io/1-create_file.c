a9e496a98c63c3f67041af3b331485a7ffa8bc0b
chalebe1 <angeloebuks@gmail.com>
2023-02-20 21:08:40 -0800

    task 1 done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (o == -1)
 		return (-1);
 	w = write(o,text_content,strlen(text_content));
-		if (w < 0)
+		if (w != strlen(text_content))
 		{
 			close(o);
 			return (-1);
