f82573fc5f6439d557fe96e52c11e14d5237408d
chalebe1 <angeloebuks@gmail.com>
2023-03-02 11:54:42 -0800

    task 0 done

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,4 @@
 #include "variadic_functions.h"
-
 /**
  *sum_them_all - sum all arguments in the function
  *
@@ -22,6 +21,5 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_end(ap);
 	return (sum);
-
 }
 
